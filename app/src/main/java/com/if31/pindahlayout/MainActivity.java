b69a1e89efbe05d72b912befab330e248ca51e61
BebinPaula <bebinpaula11@gmail.com>
2022-10-03 10:43:48 +0700

    Add 2

--- app/src/main/java/com/if31/pindahlayout/MainActivity.java
+++ app/src/main/java/com/if31/pindahlayout/MainActivity.java
@@ -2,13 +2,27 @@ package com.if31.pindahlayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnPindah;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnPindah = findViewById(R.id.btn_pindah);
+
+        btnPindah.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent pindah = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(pindah);
+            }
+        });
     }
 }
