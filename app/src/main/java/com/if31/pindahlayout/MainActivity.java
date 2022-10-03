000556deb6dc39a5321664e6737d0ef744203cc3
BebinPaula <bebinpaula11@gmail.com>
2022-10-03 11:20:45 +0700

    Menuju ke selanjutnya

--- app/src/main/java/com/if31/pindahlayout/MainActivity.java
+++ app/src/main/java/com/if31/pindahlayout/MainActivity.java
@@ -6,18 +6,22 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     Button btnPindah;
+    EditText etNama;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         getSupportActionBar().setTitle("layout A");
 
         btnPindah = findViewById(R.id.btn_pindah);
+        etNama = findViewById(R.id.et_nama);
 
         btnPindah.setOnClickListener(new View.OnClickListener() {
             @Override
